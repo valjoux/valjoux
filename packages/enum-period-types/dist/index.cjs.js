d76e3466e0f0097ddd7c6b2811997ae8d2d9580d
alkashi <hoyeungw@outlook.com>
2020-04-14 09:54:46 +0800

    add @valjoux/enum-period-types

---
+++ packages/enum-period-types/dist/index.cjs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const DAILY = 'd',
+      WEEKLY = 'w',
+      MONTHLY = 'm',
+      QUARTERLY = 'q',
+      YEARLY = 'y',
+      PENTALY = 'p';
+
+exports.DAILY = DAILY;
+exports.MONTHLY = MONTHLY;
+exports.PENTALY = PENTALY;
+exports.QUARTERLY = QUARTERLY;
+exports.WEEKLY = WEEKLY;
+exports.YEARLY = YEARLY;
---
+++ packages/enum-period-types/dist/index.esm.js
@@ -0,0 +1,8 @@
+const DAILY = 'd',
+      WEEKLY = 'w',
+      MONTHLY = 'm',
+      QUARTERLY = 'q',
+      YEARLY = 'y',
+      PENTALY = 'p';
+
+export { DAILY, MONTHLY, PENTALY, QUARTERLY, WEEKLY, YEARLY };
