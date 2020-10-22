9d6da7b4956abae9148f74970fe3ae3e7c85f9a8
hazen <hoyeungw@outlook.com>
2020-10-22 14:35:11 +0800

    update dependencies

--- packages/timestamp-pretty/test/Timestamp.test.js
+++ packages/timestamp-pretty/test/Timestamp.test.js
@@ -3,7 +3,7 @@ import { Timestamp }            from '../src/Timestamp'
 import { init }                 from '@vect/vector'
 import { delogger, logger }     from '@spare/logger'
 import { timeout }              from '@valjoux/timeout'
-import { rand, randIntBetw }    from '@aryth/rand'
+import { rand, randBetw }       from '@aryth/rand'
 import { dateTime }             from '../index'
 
 const timestamp = Timestamp.build(
@@ -17,9 +17,9 @@ const ticks = init(16, x => x)
 ticks |> delogger
 
 const randDate = () => new Date(
-  randIntBetw(800, 2030),
+  randBetw(800, 2030),
   rand(12),
-  randIntBetw(1, 31),
+  randBetw(1, 31),
   rand(23),
   rand(60),
   rand(60),
