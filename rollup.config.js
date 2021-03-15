10c3e05e64eb89fa5f9dbc743ac45b07730d30a5
hazen <hoyeungw@outlook.com>
2021-03-15 15:56:01 +0800

    migrate to berry

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs       from '@rollup/plugin-commonjs'
 import json           from '@rollup/plugin-json'
 import nodeResolve    from '@rollup/plugin-node-resolve'
 import { decoObject } from '@spare/logger'
-import babel          from 'rollup-plugin-babel'
+import babel          from '@rollup/plugin-babel'
 import fileInfo       from 'rollup-plugin-fileinfo'
 
 const { name, dependencies, main, module } = require(process.cwd() + '/package.json')
@@ -15,6 +15,7 @@ const babelPluginOptions = {
   comments: true,
   sourceMap: true,
   exclude: 'node_modules/**',
+  babelHelpers: 'bundled',
   plugins: [
     ['@babel/plugin-transform-runtime', { helpers: false, }],
     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
