cc62ba3448c5d93906626ef1481d97f4d3eb8d30
Hoyeung <hoyeungw@outlook.com>
2021-06-17 17:34:15 +0800

    update dependencies

--- packages/util-month-days/dist/index.esm.js
+++ packages/util-month-days/dist/index.esm.js
@@ -10,7 +10,7 @@ const bigMonth = m => m % 2 ^ m >= 8;
  * @returns {*}
  */
 
-const monthDays = (y, m, islp) => m === 0x2 ? 28 + (islp !== null && islp !== void 0 ? islp : isLeap(y)) : 30 + bigMonth(m);
+const monthDays = (y, m, islp) => m === 0x2 ? 28 + (islp ?? isLeap(y)) : 30 + bigMonth(m);
 
 const seasonLast = m => ~~((m - 1) / 3 + 1) * 3;
 const monthToSeason = m => ~~((m - 1) / 3 + 1);
