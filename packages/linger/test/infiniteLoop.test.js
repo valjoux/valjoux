769057a30ae943045a90c384fe6b80913be049f0
Hoyeung <hoyeungw@outlook.com>
2021-06-17 17:35:27 +0800

    improved test

--- packages/linger/test/infiniteLoop.test.js
+++ packages/linger/test/infiniteLoop.test.js
@@ -1,7 +1,12 @@
-import { decoSamples, logger } from '@spare/logger'
-import si                      from 'systeminformation'
-import { AsyncLooper }         from '../src/asyncLooper'
+import { says }        from '@palett/says'
+import { decoSamples } from '@spare/logger'
+import { time }        from '@valjoux/timestamp-pretty'
+import si              from 'systeminformation'
+import { AsyncLooper } from '../src/asyncLooper'
 
+says['processes'].attach(time)
 
 const asyncLooper = AsyncLooper.build(si.processes)
-asyncLooper.setInterval(3000, data => data.list.slice(0, 10) |> decoSamples |> logger).then()
+asyncLooper
+  .setInterval(3000, data => data.list.slice(0, 10) |> decoSamples |> says['processes'])
+  .then()
