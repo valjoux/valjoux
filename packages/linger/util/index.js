0e63443ab0c9eaabad576de6d2bfff431f013d91
Hoyeung <hoyeungw@outlook.com>
2021-06-21 10:55:26 +0800

    add intime, ontime and overtime

---
+++ packages/linger/util/index.js
@@ -0,0 +1,37 @@
+import { timeout } from '@valjoux/timeout'
+
+// wait for at least ms and return value. (no earlier than ontime time)
+//
+export function overtime(ms, fn, args) {
+  return new Promise((pass, veto) => {
+    let st = 0, rs
+    Promise.resolve(fn?.apply(this, args)).then((x) => st++ ? pass(x) : rs = x, veto)
+    Promise.resolve(timeout(ms)).then(() => st++ ? pass(rs) : void 0, veto)
+  })
+}
+
+// wait for at most ms and return value or default. (immediate or no later than ontime time)
+export function intime(ms, fn, args, df) {
+  return new Promise((pass, veto) => {
+    Promise.resolve(fn?.apply(this, args)).then((x) => pass(x), veto)
+    Promise.resolve(timeout(ms)).then(() => pass(df), veto)
+  })
+}
+
+// wait for exact ms and return value or default. (at ontime time)
+export function ontime(ms, fn, args, df) {
+  return new Promise((pass, veto) => {
+    let st = 0, rs
+    Promise.resolve(fn?.apply(this, args)).then((x) => ( rs = x, st++ ), veto)
+    Promise.resolve(timeout(ms)).then(() => pass(st ? rs : df), veto)
+  })
+}
+
+export function* infinite(vec) {
+  const hi = vec.length
+  let lo = 0
+  while (true) {
+    yield vec[lo++]
+    if (lo === hi) {lo = 0}
+  }
+}
