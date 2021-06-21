9bf07b3fcb9908002c38afb1cf2b7d1b5151bb67
Hoyeung <hoyeungw@outlook.com>
2021-06-21 10:56:13 +0800

    changed api accordingly

--- packages/linger/src/linger.js
+++ packages/linger/src/linger.js
@@ -1,19 +1,6 @@
-import { timeout } from '@valjoux/timeout'
+import { overtime } from '../util'
 
 export function linger(ms, fn, ...args) {
-  return awaitToPromise.call(this, ms, fn, args)
-}
-
-export function awaitToPromise(ms, fn, args) {
-  const self = this
-  return new Promise((pass, veto) => {
-    let st = false, rs
-    Promise
-      .resolve(fn.apply(self, args))
-      .then(x => st++ ? pass(x) : rs = x, veto)
-    Promise
-      .resolve(timeout(ms))
-      .then(_ => { if (st++) pass(rs) }, veto)
-  })
+  return overtime.call(this, ms, fn, args)
 }
 
