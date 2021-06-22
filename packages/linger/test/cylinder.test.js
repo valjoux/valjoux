188de1f704ac122ce6a2dd94f661f1e663f7bdc8
Hoyeung <hoyeungw@outlook.com>
2021-06-22 11:52:38 +0800

    fix bugs

--- packages/linger/test/cylinder.test.js
+++ packages/linger/test/cylinder.test.js
@@ -1,4 +1,4 @@
-import { rand }     from '@aryth/rand'
+import { randBetw } from '@aryth/rand'
 import { says }     from '@palett/says'
 import { AEU }      from '@spare/enum-chars'
 import { decoFlat } from '@spare/logger'
@@ -14,11 +14,11 @@ async function mem() {
   return mapper(await si.mem(), n => ( n / 1024 ) >> 10)
 }
 
-async function lazyRand() {
-  return linger(1200, function getRand() { return rand(10) })
+async function lazyRand(lo, hi) {
+  return linger(800, function getRand() { return randBetw(lo, hi) })
 }
 
-const cylinder = Cylinder.from(lazyRand, si.cpuCurrentSpeed, si.cpuTemperature, mem, lazyRand)
+const cylinder = Cylinder.from(lazyRand, si.cpuCurrentSpeed, si.cpuTemperature, mem, { fn: lazyRand, arg: [ 10, 20 ] })
 cylinder.default = AEU
 cylinder
   .setInterval(1000, data => data |> decoFlat |> says['processes'])
