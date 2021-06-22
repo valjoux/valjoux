2edca990c688e130ac961b05b7a66ea17ad3d630
Hoyeung <hoyeungw@outlook.com>
2021-06-22 17:36:08 +0800

    fix bugs

--- packages/timeout/src/timings.js
+++ packages/timeout/src/timings.js
@@ -1,9 +1,10 @@
 import { valid }   from '@typen/nullish'
-import { timeout } from '../dist/index.esm'
+import { timeout } from './timeout'
+
 
 export function overtime(ms, fn, arg) {
   return new Promise((pass, veto) => {
-    let st = 0, rs
+    let rs, st = 0
     Promise.resolve(valid(arg) ? fn(arg) : fn()).then((x) => st++ ? pass(x) : rs = x, veto)
     Promise.resolve(timeout(ms)).then(() => st++ ? pass(rs) : void 0, veto)
   })
@@ -12,16 +13,15 @@ export function overtime(ms, fn, arg) {
 // wait for at most ms and return value or default. (immediate or no later than ontime time)
 export function intime(ms, fn, arg, df) {
   return new Promise((pass, veto) => {
-    let rs = df
-    Promise.resolve(valid(arg) ? fn(arg) : fn()).then((x) => pass(rs = x), veto)
-    Promise.resolve(timeout(ms)).then(() => pass(rs), veto)
+    Promise.resolve(valid(arg) ? fn(arg) : fn()).then((x) => pass(df = x), veto)
+    Promise.resolve(timeout(ms)).then(() => pass(df), veto)
   })
 }
 
 // wait for exact ms and return value or default. (at ontime time)
 export function ontime(ms, fn, arg, df) {
   return new Promise((pass, veto) => {
-    let st = 0, rs
+    let rs, st = 0
     Promise.resolve(valid(arg) ? fn(arg) : fn()).then((x) => ( rs = x, st++ ), veto)
     Promise.resolve(timeout(ms)).then(() => pass(st ? rs : df), veto)
   })
