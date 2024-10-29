39db79d63a7f4e9e13d96e2024ad80937d3ed54c
hoyeungw <hoyeungw@outlook.com>
2024-10-29 22:47:08 +0800

    rename *.test.js to *.unit.js

--- packages/timestamp-pretty/test/functions.test.js
+++
@@ -1,6 +0,0 @@
-import { date, dateTime, roughTime, time } from '../index'
-
-date() |> console.log
-time() |> console.log
-roughTime() |> console.log
-dateTime() |> console.log
--- packages/util-ymd/test/calibre.test.js
+++
@@ -1,6 +0,0 @@
-import { logger, xr } from '@spare/logger'
-import { calibre } from '../src/calibre'
-
-for (let y = 2020, m = 2, d = 31, ymd; m < 12; m++) {
-  xr()['ymd'](ymd = [y, m, d]).calibre(calibre(ymd, 30)) |> logger
-}
